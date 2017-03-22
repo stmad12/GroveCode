b138c334213f9a8f5d13790d684bf95fec9a8586
Sean Sollars <seansollars@amdg.rockhursths.edu>
2017-03-22 11:12:24 -0500

    ss

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+var http = require('http');
+var socketio = require('socket.io');
 var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
-
-var express = require('express');
+var server = http.createServer(app);
+var io = socketio(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -11,10 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
 app.post('/', function(req,res) {
     console.log('post / =' + JSON.stringify(req.body));
+    io.sockets.emit('echo', req.body);
     res.status(200).send('got it');
 });
-app.listen(3000, function () {
+
+server.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
