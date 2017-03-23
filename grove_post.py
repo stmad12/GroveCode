3ea19ec31c8ca16a4556aec57e58f79f4f1303e7
stmad12 <skmskis@gmail.com>
2017-03-23 11:58:49 -0500

    sensor now pushes to server

--- grove_post.py
+++ grove_post.py
@@ -4,7 +4,7 @@ import grovepi
 import sys
 
 touch_sensor = 4                           # DHT22 temp & humidity sensor is connected to port D8
-url = "http://10.10.98.173:3000/"
+url = "http://10.10.89.3:3000/echo"
 
 grovepi.pinMode(touch_sensor,"INPUT")
 
@@ -13,21 +13,20 @@ while True:
   try:
     print(grovepi.digitalRead(touch_sensor))
 
-    sleepval = grovepi.digitalRead(touch_sensor)
+    sleep_val = grovepi.digitalRead(touch_sensor)
 
 
-    payload = { sleepval }
+    payload = { 'sleep' : sleep_val }
 
-    requests.post(sleepval, data=payload)    # write data
+    requests.post(url, data=payload)    # write data
 
-    time.sleep(2.0)           # 2 second delay
+    time.sleep(.25)           # 2 second delay
 
   except KeyboardInterrupt:
     print "Terminating"
     break
   except IOError:
-    print "IOError, continuing"
+    print "Push/Pull Error, continuing"
   except:
     print "Unexpected error, continuing"
     print "sys.exc_info()[0]: ", sys.exc_info()[0]
-
